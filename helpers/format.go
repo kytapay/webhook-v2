6451aadc569ddfb116d52bb2f203899a6aef72d6
CiroosAI <ciroosai@gmail.com>
2025-11-24 08:42:07 +0700

    Commit

--- helpers/format.go
+++ helpers/format.go
@@ -3,9 +3,10 @@ package helpers
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-// FormatNumber formats number with thousand separators
+// FormatNumber formats number with thousand separators (using dot)
 func FormatNumber(num interface{}, decimals int) string {
 	var numFloat float64
 	switch v := num.(type) {
@@ -25,7 +26,54 @@ func FormatNumber(num interface{}, decimals int) string {
 		return "0"
 	}
 
+	// Format with decimals
 	format := fmt.Sprintf("%%.%df", decimals)
-	return fmt.Sprintf(format, numFloat)
+	formatted := fmt.Sprintf(format, numFloat)
+
+	// Split integer and decimal parts
+	parts := strings.Split(formatted, ".")
+	integerPart := parts[0]
+	decimalPart := ""
+	if len(parts) > 1 {
+		decimalPart = parts[1]
+	}
+
+	// Add thousand separators (dots) to integer part from right to left
+	// Reverse the string, add dots every 3 digits, then reverse back
+	reversed := reverseString(integerPart)
+	var formattedReversed strings.Builder
+	for i, char := range reversed {
+		if i > 0 && i%3 == 0 {
+			formattedReversed.WriteString(".")
+		}
+		formattedReversed.WriteRune(char)
+	}
+	integerPart = reverseString(formattedReversed.String())
+
+	// Add decimal part if exists
+	if decimalPart != "" {
+		result.WriteString(",")
+		result.WriteString(decimalPart)
+	}
+
+	var result strings.Builder
+	result.WriteString(integerPart)
+
+	// Add decimal part if exists
+	if decimalPart != "" {
+		result.WriteString(",")
+		result.WriteString(decimalPart)
+	}
+
+	return result.String()
+}
+
+// reverseString reverses a string
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
